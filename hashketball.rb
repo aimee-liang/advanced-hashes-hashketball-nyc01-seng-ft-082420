5415431ac7c46a3bcab3487c6f604933501d9586
Aimee Liang <aimeeeliang@gmail.com>
2020-08-15 01:54:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -155,7 +155,7 @@ end
 
 def player_numbers(team_name)
   game_hash.each do |team, team_data|
-    if team_data.has_value?(team)
+    team_data.each do |team|
     return team_data[:players].map{|player| player[:number]}
     end
 end
