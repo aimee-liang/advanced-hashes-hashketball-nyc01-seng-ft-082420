7faf019e08188ea78fc55d7a802f4a780d2f2666
Aimee Liang <aimeeeliang@gmail.com>
2020-08-15 01:30:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,7 +130,7 @@ end
 def num_points_scored(player_name)
   game_hash.each do |team|
     team[:players].each do |player|
-      return players[:points] if player.has_value?(player_name)
+      return players[:points].to_i if player.has_value?(player_name)
       end
     end
 end
