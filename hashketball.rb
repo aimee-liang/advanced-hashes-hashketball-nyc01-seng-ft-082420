76d264051cf6e1a9437c9f811bd9b25c9fe58730
Aimee Liang <aimeeeliang@gmail.com>
2020-08-15 01:19:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,11 +128,11 @@ end
 
 # Write code here
 def num_points_scored(player_name)
-  game_hash.each do |team_data|
-    team_data.each do |player|
+  game_hash.each do |team|
+    team[:players].each do |player|
       return player[:points] if player.has_value?(player_name)
+      end
     end
-  end
 end
 
 
