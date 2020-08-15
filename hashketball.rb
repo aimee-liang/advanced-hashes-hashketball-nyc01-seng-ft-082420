ceb68904ba10dc5970bd7a3381c20c75c8f59a7e
Aimee Liang <aimeeeliang@gmail.com>
2020-08-15 01:56:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -156,7 +156,9 @@ end
 def player_numbers(team_name)
   game_hash.each do |team, team_data|
     if team_data[:team_name] == team_name
-      
+      team_data.each do |key, value|
+        return 
+      end
     end
 end
 end
